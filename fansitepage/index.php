03d7004412145372ffad3d189023013538da36f3
MuhammadEWEE <168726789+MuhammadEWEE@users.noreply.github.com>
2024-06-12 15:50:25 +0200

    Add files via upload

---
+++ fansitepage/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    for ($str = "LaPlateforme";) 
+    {
+        echo "$str";
+    }
+    ?>
+    
+</body>
+</html>
